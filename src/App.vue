d5873ad8780099bb7f5ec4595b98eb32668134d5
chenmq <1075124372@qq.com>
2020-09-03 16:54:22 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App  yyyy" />
+    <HelloWorld msg="Welcome to Your Vue.js App  zz" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 };
 </script>
 
